48e119577320486e3c21e84c54c8a988c4ca5f32
Dilshan <dk95gs@gmail.com>
2018-05-07 22:24:10 -0400

    changes to weather page

--- src/components/weatherLocation/WeatherLocationPage.vue
+++ src/components/weatherLocation/WeatherLocationPage.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="container-fluid">
     <div class="page-header">
-      <h1>Cart</h1>
+      <h1>Weather</h1>
     </div>
     <h2>Work In Progress</h2>
+
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import Header from './components/shared/Header.vue'
 import Footer from './components/shared/Footer.vue'
 import NavBar from './components/shared/NavBar.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import {routes} from './routes.js'
 
@@ -11,6 +13,7 @@ const router = new VueRouter({
   routes:routes
 });
 Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
 
 Vue.component('app-header', Header);
 Vue.component('app-footer', Footer);
